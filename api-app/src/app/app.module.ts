a8dcb8391dad1333dd9bc981ce77f47c95a671ce
Samuel Han <samuelhan@samuels-air.mynetworksettings.com>
2022-08-16 10:40:08 -0400

    added css changes and ang mat

--- api-app/src/app/app.module.ts
+++ api-app/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { HttpClientModule } from '@angular/common/http';
+import {MatCardModule} from '@angular/material/card';
+
 
 import { AppComponent } from './app.component';
 import { AlbumsComponent } from './components/albums/albums.component';
@@ -27,7 +29,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatIconModule,
     BrowserAnimationsModule,
     MatExpansionModule,
-    HttpClientModule
+    HttpClientModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
